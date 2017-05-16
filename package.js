bb96e592d808cfa9a26568fe455bcafcdf5bb708
yinlianghui <yinlianghui@hotoa.com>
2017-05-16 17:03:38 +0800

    升级版本，准备发到meteor

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'steedos:autoform-modals',
   summary: "Create, update and delete collections with modals",
-  version: "0.3.8",
+  version: "0.3.9",
   git: "https://github.com/yogiben/meteor-autoform-modals"
 });
 
